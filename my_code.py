0bcd25ba3fa884eaaa800c67b3fb580b974b1df1
mishikag <23mgupta@athenian.org>
2020-11-03 13:18:27 -0800

    passed

--- my_code.py
+++ my_code.py
@@ -1,11 +1,20 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+#
 
 def avg_temp():
+    sum_list = 0
+    with open('temps.txt') as file_object:
+        line_list = file_object.readlines()
+        list_length = len(line_list) - 1
+        for i in range(1, list_length):
+            list_length = int(list_length)
+        for i in range(1, len(line_list)):
+            sum_list = sum_list + int(line_list[i])
+        final_sum = sum_list/list_length
 
-
-    return
-
+        avg_list = sum_list/(list_length)
+        avg_list = round(avg_list, 2)
+    return avg_list
 
 if __name__ == '__main__':
     print(avg_temp())
